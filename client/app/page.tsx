a13f046b78b11b6373bfb256e26a238d861bcb88
Rashika Shah <rashikashah0310@gmail.com>
2026-07-26 17:04:54 +0530

    feat: setup Express+Socket.IO server and Next.js client, verify socket connection

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,20 @@
-import Image from "next/image";
+"use client"; // needed since we're using hooks
+
+import { useEffect } from "react";
+import { socket } from "@/lib/socket";
 
 export default function Home() {
-  return (
-    <div>hii rashi</div>
-  );
+  useEffect(() => {
+    socket.connect();
+
+    socket.on("connect", () => {
+      console.log("Connected to server:", socket.id);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  return <div>PingRoom - Testing Connection</div>;
 }
